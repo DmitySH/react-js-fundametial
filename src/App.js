8528d724d701ae0dffd1dd03b30a58af57fcc4e6
dshagarov <dshagarov@ozon.ru>
2022-07-15 13:42:56 +0300

    add empty project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <strong>
+            1. Some post
+          </strong>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
